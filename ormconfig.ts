77acf0323d31d6a9167ff698804e28e9b19e8d49
Prince Narteh <prinarteh@gmail.com>
2023-04-28 04:16:39 +0000

    add configuration

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,12 +1,29 @@
-import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-export const config: MysqlConnectionOptions = {
-  type: 'mysql',
-  username: 'root',
-  password: 'root',
-  database: 'mimallgh',
-  host: 'localhost',
-  port: 3306,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_DATABASE'),
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleAsyncOptions> =>
+    TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService],
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { config } from 'ormconfig';
+import { typeOrmConfigAsync } from 'ormconfig';
 import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 import { ProductModule } from './product/product.module';
+import { ShopAuthModule } from './shop-auth/shop-auth.module';
 import { ShopModule } from './shop/shop.module';
 import { UserModule } from './user/user.module';
-import { ShopAuthModule } from './shop-auth/shop-auth.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
     AuthModule,
     UserModule,
     ProductModule,
     ShopModule,
-    TypeOrmModule.forRoot(config),
     ShopAuthModule,
     OrdersModule,
   ],
